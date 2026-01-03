b10a86c9b0ce5f9f9f6289704bde2c732227ea56
Sujay511 <sujunaguaru143@gmail.com>
2026-01-03 19:58:28 +0530

    Add files via upload

--- My_Text_Editor.py
+++ My_Text_Editor.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter.filedialog import askopenfile,asksaveasfilename
+from tkinter.filedialog import askopenfilename,asksaveasfilename
 
 window=Tk()
 window.title("Sujay's Text Editor")
@@ -8,14 +8,14 @@ window.rowconfigure(0,minsize=800,weight=1)
 window.columnconfigure(1,minsize=800,weight=1)
 
 def open_file():
-    filepath=askopenfile(
+    filepath=askopenfilename(
         filetypes=[("Text Files","*.txt"),("All Files","*.*")]
     )
     if not filepath:
         return
     txt_edit.delete(1.0,END)
 
-    with open(filepath,"w") as input_file:
+    with open(filepath,"r") as input_file:
         text=input_file.read()
         txt_edit.insert(END,text)
         input_file.close()
@@ -38,8 +38,8 @@ btn_save=Button(fr_buttons,text="Save As",command=save_file)
 btn_open.grid(row=0,column=0,sticky="ew",padx=5,pady=5)
 btn_save.grid(row=1,column=0,sticky="ew",padx=5)
 
-fr_buttons.grid(row=0,column=0)
-txt_edit.grid(row=0,column=1)
+fr_buttons.grid(row=0,column=0,sticky="ns")
+txt_edit.grid(row=0,column=1,sticky="nsew")
 
 window.mainloop()
 
